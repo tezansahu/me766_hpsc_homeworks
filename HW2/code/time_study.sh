5e7d07a943b847820507932d58d724917c6a8049
tezansahu <tezansahu@gmail.com>
2021-04-04 00:24:52 +0530

    time study for mpi program done

--- HW2/code/time_study.sh
+++ HW2/code/time_study.sh
@@ -18,3 +18,27 @@ do
     { echo "----------------------"; } 1>> openmp_timing.txt
     { echo ; } 1>> openmp_timing.txt
 done
+
+##############################################################################################
+
+ulimit -s unlimited
+mpic++ mat_mul_upper_mpi.cpp
+
+{ echo "MPI Parallelization:" ; } 1> mpi_timing.txt
+{ echo "--------------------------------------"; } 1>> mpi_timing.txt
+for n in 100 500 1000 2000 5000
+do
+    { echo "Matrix Dimension:" $n ; } | tee -a mpi_timing.txt
+    { echo "----------------------"; } 1>> mpi_timing.txt
+    for num_proc in 1 2 4 6 8
+    do
+        { echo "Number of Processes: " $num_proc ; } 1>> mpi_timing.txt
+
+        { mpirun -np $num_proc ./a.out $n; } 1>> mpi_timing.txt
+
+
+        { echo ; } 1>> mpi_timing.txt
+    done
+    { echo "----------------------"; } 1>> mpi_timing.txt
+    { echo ; } 1>> mpi_timing.txt
+done
